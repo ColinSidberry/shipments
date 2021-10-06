141c75e7552ce906068a795ed5827725afdd74bd
jeanne <novasquared@yahoo.com>
2021-10-05 20:11:40 -0500

    started work on mocking axios

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,17 +1,53 @@
 "use strict";
 
+const AxiosMockAdapter = require(
+  "axios-mock-adapter");
+const axios = require("axios");
+const axiosMock = new AxiosMockAdapter(axios);
+
 const {
-  shipProduct,
+  shipProduct, SHIPIT_SHIP_URL, SHIPIT_API_KEY
 } = require("./shipItApi");
 
 
-test("shipProduct", async function () {
-  const shipId = await shipProduct({
+// test("shipProduct", async function () {
+//   const shipId = await shipProduct({
+//     productId: 1000,
+//     name: "Test Tester",
+//     addr: "100 Test St",
+//     zip: "12345-6789",
+//   });
+
+//   expect(shipId).toEqual(expect.any(Number));
+// });
+
+
+test("shipIt API Test", async function () {
+
+  axiosMock.onPost(SHIPIT_SHIP_URL, {
+    itemId: 1000,
+    name: "Test Tester",
+    addr: "100 Test St",
+    zip: "12345-6789", 
+    key: SHIPIT_API_KEY
+  })
+    .reply(200, {
+      "receipt": {"shipId": 1628}
+    });
+
+  const res = await shipProduct({
     productId: 1000,
     name: "Test Tester",
     addr: "100 Test St",
-    zip: "12345-6789",
+    zip: "12345-6789"
   });
-
-  expect(shipId).toEqual(expect.any(Number));
+  expect(res).toEqual({
+    "shipped": {
+      itemId: 1000,
+      name: 'Test Tester',
+      addr: '100 Test St',
+      zip: '12345-6789',
+      shipId: 1628
+    }
+  });
 });
